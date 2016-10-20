1ae297acfe1a799acd311b8079608aecf64dad5d
John Gregory <jgregory@companieshouse.gov.uk>
2016-10-20 17:41:00 +0100

    Refactors test to goconvey

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -3,20 +3,17 @@ package spec
 import (
 	"testing"
 
-	"github.com/necrophonic/goat/spec"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestImportSpecFile(t *testing.T) {
 
-	s := spec.Import("../testing/simple-1.test.yml")
+	Convey("Simple spec file correctly imported", t, func() {
+		s := Import("../testing/simple-1.test.yml")
 
-	if s.Name != "Simple test 1" {
-		t.Error("Test name not as expected")
-	}
-	if s.Description != "A simple test layout" {
-		t.Error("Description not as expected")
-	}
-	if len(s.Scenarios) != 2 {
-		t.Error("Expected two scenarios")
-	}
+		So(s.Name, ShouldEqual, "Simple test 1")
+		So(s.Description, ShouldEqual, "A simple test layout")
+		So(len(s.Scenarios), ShouldEqual, 2)
+
+	})
 }
