0d8cce93841e4896d2c280676d8a1b2cbca0fb9c
John Gregory <jgregory@companieshouse.gov.uk>
2016-10-20 16:59:55 +0100

    Implement goconvey for unit testing

--- initialise/initialise_test.go
+++ initialise/initialise_test.go
@@ -3,21 +3,29 @@ package initialise
 import (
 	"testing"
 
-	"github.com/necrophonic/assert"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 const (
 	errStub = "folder not ready to initialise: "
 )
 
-func TestInitialiseFolderDoesNotExist(t *testing.T) {
-	assert.ErrorEquals(Folder("./invalid"), errStub+"target [./invalid] does not exist", t)
-}
+func TestInitialiseFolder(t *testing.T) {
+	Convey("Folder should not exist", t, func() {
+		err := Folder("./invalid")
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldEqual, errStub+"target [./invalid] does not exist")
+	})
 
-func TestInitialiseFolderIsNotEmpty(t *testing.T) {
-	assert.ErrorEquals(Folder("."), errStub+"target [.] is not empty", t)
-}
+	Convey("Folder is not empty", t, func() {
+		err := Folder(".")
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldEqual, errStub+"target [.] is not empty")
+	})
 
-func TestInitialiseFolderIsNotAFolder(t *testing.T) {
-	assert.ErrorEquals(Folder("./initialise.go"), errStub+"target [./initialise.go] is not a folder", t)
+	Convey("Target is not a folder", t, func() {
+		err := Folder("./initialise.go")
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldEqual, errStub+"target [./initialise.go] is not a folder")
+	})
 }
